18438bb9cbcaf52301450004ec3a43ed9b2b85e0
DeepeshIITdh <dsh.2065@gmail.com>
2025-07-19 15:01:05 +0530

    commit

--- backend/utils.py
+++ backend/utils.py
@@ -1,24 +1,14 @@
-from transformers import AutoTokenizer, AutoModelForCausalLM, pipeline
-from peft import PeftModel
-import torch
-import os
+from transformers import pipeline
 from langchain_huggingface import HuggingFacePipeline
+import os
+
+model_path = os.path.join('models', 'tinyllama_finetuned_on_quizgen')
 
 def load_tinyllama_pipeline():
-    base_model_name = "TinyLlama/TinyLlama-1.1B-Chat-v1.0"
-    adapter_path = os.path.join('models', 'tinyllama_finetuned_on_quizgen')
-
-    tokenizer = AutoTokenizer.from_pretrained(base_model_name)
-    base_model = AutoModelForCausalLM.from_pretrained(base_model_name, torch_dtype=torch.float16)
-    
-    # Apply LoRA adapter
-    model = PeftModel.from_pretrained(base_model, adapter_path)
-    model.eval()
-
     pipe = pipeline(
         "text-generation",
-        model=model,
-        tokenizer=tokenizer,
+        model=model_path,
+        tokenizer=model_path,
         max_length=200,
         temperature=0.7,
         top_k=50,
@@ -26,5 +16,4 @@ def load_tinyllama_pipeline():
         repetition_penalty=1.1,
         do_sample=True
     )
-
     return HuggingFacePipeline(pipeline=pipe)
